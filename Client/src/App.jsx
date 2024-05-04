8ea886361faa0af7478a73313e67d22477db48b4
Mohitpuri Goswami <113592385+mohit5723@users.noreply.github.com>
2024-05-04 22:48:14 +0530

    Update App.jsx

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -128,7 +128,7 @@ const App = () => {
     const username = result.value;
     setPlayerName(username);
 
-    const newSocket = io("http://localhost:3000", {
+    const newSocket = io("https://tic-tac-toe-multi-7xtl.vercel.app/", {
       autoConnect: true,
     });
 
